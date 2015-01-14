b86cafb2c3de2284825b409df0ba340b6665945f
canon4444 <haruharu724@hotmail.com>
2015-01-14 14:54:46 +0900

    Create practicce2-2.c

---
+++ practicce2-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char namae[10];
+
+    printf("見習い魔術師イーノ最初の受難\n\n");
+
+    printf("キャラクターの名前を入力してください: ");
+    scanf("%s", namae);
+
+    return 0;
+}
