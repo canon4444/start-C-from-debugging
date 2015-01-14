272639ff0da9fcf93ea4b86cefb815dd39808649
canon4444 <haruharu724@hotmail.com>
2015-01-14 14:56:13 +0900

    Create program.c

---
+++ program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //char name[10];
+    char name[256]; //安全のため
+
+    printf("見習い魔術師イーノ最初の受難\n\n");
+
+    printf("キャラクターの名前を入力してください: ");
+    scanf("%s", name);
+
+    return 0;
+}
