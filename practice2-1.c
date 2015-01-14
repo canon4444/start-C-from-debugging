a1fcf1c63500a70973bac1846fc41a5422065437
canon4444 <haruharu724@hotmail.com>
2015-01-14 14:54:28 +0900

    Create practice2-1.c

---
+++ practice2-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char namae[256];
+
+    printf("見習い魔術師イーノ最初の受難\n\n");
+
+    printf("キャラクターの名前を入力してください: ");
+    scanf("%s", name);
+
+    return 0;
+}
